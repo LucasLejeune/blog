23a28a08b6978cb3fd222ba3eef33b8d9d2b750d
Lucas Lejeune <lucaslejeune0@gmail.com>
2022-10-29 21:25:14 +0200

    Page catégorie

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -6,17 +6,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CategorieRepository;
+use App\Repository\ArticleRepository; 
 
 class CategorieController extends AbstractController
 {
     /**
-     * @Route("/categories", name="app_categorie")
+     * @Route("/categories", name="app_categories")
      */
     public function index(CategorieRepository $categorieRepo): Response
     {
-        $categories = $categorieRepo->findAll();;
+        $categories = $categorieRepo->findAll();
         return $this->render('categorie/index.html.twig', [
             'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * @Route("/categorie/{id<\d+>}", name="app_categorie")
+     */
+    public function categorie($id,CategorieRepository $categorieRepo, ArticleRepository $articleRepo): Response
+    {
+        $articles = $articleRepo->findBy(["categorie" => $id]);
+        return $this->render('categorie/categorie.html.twig', [
+            'articles' => $articles,
         ]);
     }
 }
